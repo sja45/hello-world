a3b52bd872dede1b9de8336829a49714565b363e
Sunny Jeng <sunnyjeng@d142-58-115-173.burnaby.sfu.ca>
2017-07-31 16:46:40 -0700

    -a

--- greetings/views.py
+++ greetings/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hi Eugean!")
+	return HttpResponse("Hi Eugean! Remember to walk today :D")
